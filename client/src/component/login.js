9f5b669250ee928219249da90a85a93b8d94bb1e
Balmiki Padhyaya <bfv12@txstate.edu>
2024-02-21 13:18:40 -0600

    Created css file for login page and added login function

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -1,10 +1,30 @@
+import react, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { login } from "../auth";
+import { useHistory } from "react-router-dom";
+import "./App.css";
 
-const login=()=>{
-    return(
-        <div className='login'>
-            <h1>login Page</h1>
-        </div>
-    )
-}
+const login = () => {
+  const [formData, setData] = useState({ username: "", password: "" });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-export default login
+    const response = await fetch("https://localhost:3000/login", {
+      method: "POST",
+      headers: { "content-type": "application/JSON" },
+      body: JSON.stringify(formData),
+    });
+  };
+  return (
+    <div className="container">
+      <div className="login-form">
+        <h1>login Page</h1>
+        <form></form>
+      </div>
+    </div>
+  );
+};
+
+export default login;
