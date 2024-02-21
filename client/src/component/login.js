83eebc67077c1a484f97f3ad26ec337a32125aca
Balmiki Padhyaya <bfv12@txstate.edu>
2024-02-21 15:23:42 -0600

    Added text fields for login

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -16,12 +16,47 @@ const login = () => {
       headers: { "content-type": "application/JSON" },
       body: JSON.stringify(formData),
     });
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log("Login successful");
+    } else {
+      console.log("Login failed", data.console.error());
+    }
   };
   return (
     <div className="container">
       <div className="login-form">
-        <h1>login Page</h1>
-        <form></form>
+        <h1>Login Page</h1>
+        <form>
+          {" "}
+          onSubmit = {handleSubmit}
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="username or email"
+              value={formData.username}
+              onChange={(e) =>
+                setData({ ...formData, username: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Your password"
+              value={formData.password}
+              onChange={(e) =>
+                setData({ ...formData, password: e.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button type="submit" variant="primary"></Button>
+          </Form.Group>
+        </form>
       </div>
     </div>
   );
