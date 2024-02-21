396ee738436bafd0227d7b801e17d8d24b708b7c
Keshav Adhikari <k_a354@txstate.edu>
2024-02-20 20:55:26 -0600

    created a wellness function to add multiple activities

---
+++ client/src/wellnesspage.js
@@ -0,0 +1,10 @@
+import React,{useState} from "react";
+
+function wellnessTab(){
+    const[activities, setActivities]=useState([
+        {id:1, type:'article',title:'The Benefits of Meditation',category:'Health Benefits',link:''},
+        {id:1, type:'article',title:'The Benefits of Meditation',category:'Health Benefits',link:''},
+        {id:1, type:'article',title:'The Benefits of Meditation',category:'Health Benefits',link:''},
+        {id:1, type:'article',title:'The Benefits of Meditation',category:'Health Benefits',link:''},
+    ]);
+}
