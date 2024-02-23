ddf338251e6cf446a4f42ca7763f9bae2ba499b4
Vishal Manral <trq7@txstate.edu>
2024-02-22 23:35:23 -0600

    fixed the signup page design and made the link to login page work

--- client/src/component/signup.js
+++ client/src/component/signup.js
@@ -1,40 +1,46 @@
-import React from "react";
-import "./signup.css";
-const signup = () => {
-  return (
-    <div className="container">
-      <div className="header">
-        <div className="text">SIGN UP</div>
-        <div className="underline"></div>
-      </div>
-      <div className="inputs">
-        <div className="input">
-          <img src="" alt="" />
-          <label htmlFor="username">USERNAME</label>
-          <input type="text" placeholder="Enter your username here" />
+import React from 'react';
+import './signup.css';
+import { Link } from 'react-router-dom';
+import user_icon from './img/person.png';
+import email_icon from './img/email.png';
+import password_icon from './img/password.png';
+const signup=()=>{
+    return(
+        <div className='container'>
+            <div className="header">
+                <div className='text'>
+                    SIGN UP
+                </div>
+                <div className='underline'></div>
+            </div>
+                <div className="inputs">
+                    <div className='input'>
+                        <label htmlFor="username"><img src={user_icon} alt="user name" /></label>
+                        <input type = "text" id="username" placeholder='Enter your username here' />
+                    </div>
+                    <div className='input'>
+                        <label htmlFor="email"><img src={email_icon} alt="email id" /></label>
+                        <input type = "email" id="email" placeholder='Enter a valid email'/>
+                    </div>
+                    <div className='input'>
+                        <label htmlFor="password"><img src={password_icon} alt="password" /></label>
+                        <input type = "password" id="password" placeholder='Enter your password' />
+                    </div>
+                    <div className='input'>
+                        <label htmlFor="confirmpassword"><img src={password_icon} alt="password" /></label>
+                        <input type = "password" id="confirmpassword" placeholder='Confirm your password' />
+                    </div>
+            </div>
+            <div className="submitContainer">
+                <div className="submit">SIGN UP</div>
+            </div>
+            <div className="login">Already a user? <span> <Link to="/login">Click here to login</Link></span></div>
         </div>
-        <div className="input">
-          <img src="" alt="" />
-          <label htmlFor="username">EMAIL</label>
-          <input type="email" placeholder="Enter a valid email" />
-        </div>
-        <div className="input">
-          <img src="" alt="" />
-          <label htmlFor="username">PASSWORD</label>
-          <input type="password" placeholder="Enter your password" />
-        </div>
-        <div className="input">
-          <img src="" alt="" />
-          <label htmlFor="username">CONFIRM PASSWORD</label>
-          <input type="password" placeholder="Enter your password again" />
-        </div>
-      </div>
-      <div className="submitContainer">
-        <div className="submit">SIGN UP</div>
-        {/* <div className="submit">Go To Login</div> */}
-      </div>
-    </div>
-  );
-};
+    )
+}
+
+
+
+
 
 export default signup;
