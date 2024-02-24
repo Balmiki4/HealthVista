bfd7d80e32375dd296ae259b9e476b158c4ca4b2
Balmiki Padhyaya <bfv12@txstate.edu>
2024-02-24 11:33:15 -0600

    Added forgot password and remember me button.

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
 import "./login.css";
+import user_icon from "./img/person.png";
+import password_icon from "./img/password.png";
 
 const LoginPage = () => {
   const [formData, setData] = useState({ username: "", password: "" });
@@ -32,39 +34,58 @@ const LoginPage = () => {
 
         <form onSubmit={handleSubmit}>
           <div className="inputs">
-            <Form.Group>
-              <Form.Label>Username</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Username or Email"
-                value={formData.username}
-                onChange={(e) =>
-                  setData({ ...formData, username: e.target.value })
-                }
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Your password"
-                value={formData.password}
-                onChange={(e) =>
-                  setData({ ...formData, password: e.target.value })
-                }
-              />
-            </Form.Group>
+            <div className="input">
+              <label htmlFor="username">
+                <img src={user_icon} alt="user name" />
+              </label>
+              <Form.Group>
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Username or Email"
+                  value={formData.username}
+                  onChange={(e) =>
+                    setData({ ...formData, username: e.target.value })
+                  }
+                />
+              </Form.Group>
+            </div>
+            <div className="input">
+              <label htmlFor="password">
+                <img src={password_icon} alt="password" />
+              </label>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Your password"
+                  value={formData.password}
+                  onChange={(e) =>
+                    setData({ ...formData, password: e.target.value })
+                  }
+                />
+              </Form.Group>
+            </div>
           </div>
           <div className="login-button">
             <Form.Group>
-              <Button type="submit" variant="primary">
+              <Button type="submit" variant="primary" onClick={handleSubmit}>
                 Login
               </Button>
             </Form.Group>
           </div>
         </form>
-      </div>
-    </div>
+        <div className="remember-forgot">
+    <Form.Group>
+      <Form.Check type="checkbox" label="Remember me" />
+    </Form.Group>
+    <Link to="/forgot-password">Forgot Password?</Link>
+  </div>
+  <div className="signup-link">
+    <span>Don't have an account? </span>
+    <Link to="/signup">Sign Up</Link>
+  </div>
+  </div>
   );
 };
 
