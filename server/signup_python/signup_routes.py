489bae7b742a2a0502f2c20d8e6ffb1dcab8869d
Vishal Manral <trq7@txstate.edu>
2024-02-25 17:49:26 -0600

    Added code to hash the user's entered password before storing it
    Also added feature to check if the password is already taken

--- server/signup_python/signup_routes.py
+++ server/signup_python/signup_routes.py
@@ -1,4 +1,5 @@
 import os
+import bcrypt
 from flask import Blueprint
 from signup_python.signup_models import User
 from flask import request, jsonify
@@ -28,13 +29,23 @@ def signup():
         email = request.json.get('email')
         password = request.json.get('password')
 
-        # Check if username or email already exists
-        existing_user = users_collection.find_one({'$or': [{'username': username}, {'email': email}]})
-        if existing_user:
-            return jsonify({'error': 'Username or email already exists'}), 400
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
-        # Create new user object
-        new_user = User(username, email, password)
+        # Check if username or email or password already exists
+        existing_user = users_collection.find_one({'username':username})
+        if existing_user:
+            return jsonify({'error': 'Username already taken'}), 400
+        
+        existing_user_by_email = users_collection.find_one({'email': email})
+        if existing_user_by_email:
+            return jsonify({'error': 'Email already taken'}), 400
+        
+        for user in users_collection.find({}):
+            if bcrypt.checkpw(password.encode('utf-8'), user['password']):
+                return jsonify({'error': 'Password already taken'}), 400
+
+        # Create new user object and store username, email, and hashedpassword
+        new_user = User(username, email, hashed_password)
 
         # Insert new user into MongoDB
         users_collection.insert_one(new_user.__dict__)
