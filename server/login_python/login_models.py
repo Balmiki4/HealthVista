7f650c85d0fc3024e491863b0ac89ad412d38d0c
Balmiki Padhyaya <bfv12@txstate.edu>
2024-02-26 23:15:27 -0600

    Added logic for verifying user details

--- server/login_python/login_models.py
+++ server/login_python/login_models.py
@@ -8,3 +8,16 @@ client = MongoClient(uri)
 db = client['healtVista']
 users_collection = db['usersdata']
 
+class User:
+    # look up the user in database
+    def find_username(self, username):
+        return users_collection.find_one({'username': username})
+    
+    def authenticate(self, password):
+        user = self.find_username(username)
+        
+        # check if the user's password is correct
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[password]):
+            return user
+        else:
+            return None
