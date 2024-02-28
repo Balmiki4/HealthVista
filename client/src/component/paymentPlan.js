2b5cc6e0c99563d41ebe0b265bb68c0c884f2241
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-02-28 14:12:30 -0600

    added handlepayment logic in frontend

--- client/src/component/paymentPlan.js
+++ client/src/component/paymentPlan.js
@@ -1,18 +1,51 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import React, { useState, useEffect } from "react";
 import "./homePage.css";
 
+const stripePromise = loadStripe(
+  "pk_test_51OoPbHB45mq7hp813JIWoPpJikuO9iKzVOB9mpOGHdQqXneTDqBfQkoiXUvXfUUamBMLpPpbo5MgKeOGUfBwzFmE00ktXQjloo"
+);
 
-function paymentPlan(){
+const handlePayment = async (selectedPlan) => {
 
-    return(
-        <div class="conainer feature" >
-        <div class="block conainer block-plans" style={{ paddingTop: '0px' }} >
+  try {
+    const stripe = await stripePromise;
+    const response = await fetch('http://localhost:5000/get-stripe-price-id', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ selectedPlan })
+    });
+    const { priceId } = await response.json();
+
+    const { error, subscription } = await stripe.redirectToCheckout({
+        mode: "subscription",
+        lineItems: [{ price: priceId, quantity: 1 }],
+        successUrl: "http://localhost:3000/SuccessPage",
+        cancelUrl: "http://localhost:3000/cancel",
+    });
+
+    if (error) {
+      console.error("Error:", error);
+
+      // Display error message to the user here
+    }
+  } catch (error) {
+    console.error("Error in payment process:", error);
+
+    // Display error message to the user here
+  }
+};
+
+
+function paymentPlan() {
+  return (
+    <div class="conainer feature">
+      <div class="block conainer block-plans" style={{ paddingTop: "0px" }}>
         <div class="row">
           <div class="heading">
             <h1 className="secondary-heading">Thank you for registering</h1>
-            <h3 className="font-only-heading">
-              Please choose a plan
-            </h3>
+            <h3 className="font-only-heading">Please choose a plan</h3>
             <div class="separator"></div>
           </div>
           <div class="grid grid--1x3">
@@ -35,12 +68,13 @@ function paymentPlan(){
                     <li class="list__item">Basic chatbot support</li>
                   </ul>
                   <div className="plan-btn">
-                  <Link to="/">
-                        <button class="btn btn-outline-success plan-buy-now-btn">
-                          Buy Now
-                        </button>
-                      </Link>
-                    </div>
+                    <button
+                      class="btn btn-outline-success plan-buy-now-btn"
+                      onClick={()=>handlePayment('Free tier')}
+                    >
+                      Buy Now
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -66,14 +100,14 @@ function paymentPlan(){
                       <li class="list__item">Virutal Therapist</li>
                     </ul>
                     <div className="plan-btn">
-                  <Link to="/">
-                        <button class="btn btn-outline-success plan-buy-now-btn">
-                          Buy Now
-                        </button>
-                      </Link>
+                      <button
+                        class="btn btn-outline-success plan-buy-now-btn"
+                        onClick={()=>handlePayment('Pro tier')}
+                      >
+                        Buy Now
+                      </button>
                     </div>
                   </div>
-
                 </div>
               </div>
             </div>
@@ -93,29 +127,26 @@ function paymentPlan(){
                       <li class="list__item">
                         Customizable medication reminders
                       </li>
-                      <li class="list__item">
-                        Full access to wellness videos
-                      </li>
+                      <li class="list__item">Full access to wellness videos</li>
                       <li class="list__item">Expanded tracker options</li>
                     </ul>
                     <div className="plan-btn">
-                  <Link to="/">
-                        <button class="btn btn-outline-success plan-buy-now-btn">
-                          Buy Now
-                        </button>
-                      </Link>
+                      <button
+                        class="btn btn-outline-success plan-buy-now-btn"
+                        onClick={()=>handlePayment('Basic tier')}
+                      >
+                        Buy Now
+                      </button>
                     </div>
                   </div>
- 
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-    );
-
+    </div>
+  );
 }
 
 export default paymentPlan;
