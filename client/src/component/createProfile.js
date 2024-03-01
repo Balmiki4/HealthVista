cc76c8a01fb04032702626a2303e83cd58259594
Vishal Manral <trq7@txstate.edu>
2024-02-29 19:12:30 -0600

    Added JavaScript code to handle form submission event

--- client/src/component/createProfile.js
+++ client/src/component/createProfile.js
@@ -17,6 +17,49 @@ const ProfilePage = () => {
         setFormData({ ...formData, [id]: value });
     };
 
+    const [errors, setErrors] = useState({})
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        // Client-side validation
+        const errors = {};
+        if (!formData.firstName) {
+            errors.firstName = 'First Name is required';
+        }
+        if (!formData.lastName) {
+            errors.lastName = 'Last Name is required';
+        } 
+        
+        if (!formData.gender) {
+            errors.gender = 'Gender is required';
+        } 
+
+        if (!formData.dob) {
+            errors.dob = 'Date of Birth is required';
+        } 
+
+        if (!formData.gender) {
+            errors.zipCode = 'Zip code is required';
+        } 
+    
+        if (Object.keys(errors).length === 0) {
+
+            console.log('Profile successfully created!');
+            setFormData({
+                firstName: '',
+                lastName: '',
+                gender: '',
+                dob: '',
+                phoneNumber:'',
+                zipCode: '',
+            });
+        }
+        else {
+            setErrors(errors);
+        }
+};
+
     return (
         <div className="profile-container">
         <h2 className="profile-heading">Create Profile</h2>
@@ -27,9 +70,11 @@ const ProfilePage = () => {
                 type="text"
                 id="firstName"
                 className="form-input"
+                placeholder='Enter your First name here'
                 value={formData.firstName}
                 onChange={handleChange}
             />
+            {errors.firstName && <div className='error'>{errors.firstName}</div>}
             </div>
             <div className="form-group">
             <label htmlFor="lastName" className="form-label">Last Name:</label>
@@ -37,9 +82,11 @@ const ProfilePage = () => {
                 type="text"
                 id="lastName"
                 className="form-input"
+                placeholder='Enter your Last name here'
                 value={formData.lastName}
                 onChange={handleChange}
             />
+            {errors.lastName && <div className='error'>{errors.lastName}</div>}
             </div>
             <div className="form-group">
             <label htmlFor="gender" className="form-label">Gender:</label>
@@ -49,6 +96,7 @@ const ProfilePage = () => {
                 <option value="female">Female</option>
                 <option value="other">Other</option>
             </select>
+            {errors.gender && <div className='error'>{errors.gender}</div>}
             </div>
             <div className="form-group">
             <label htmlFor="dob" className="form-label">Date of Birth:</label>
@@ -59,6 +107,7 @@ const ProfilePage = () => {
                 value={formData.dob}
                 onChange={handleChange}
             />
+            {errors.dob && <div className='error'>{errors.dob}</div>}
             </div>
             <div className="form-group">
             <label htmlFor="phoneNumber" className="form-label">Phone Number:</label>
@@ -66,21 +115,25 @@ const ProfilePage = () => {
                 type="tel"
                 id="phoneNumber"
                 className="form-input"
+                placeholder='Enter your phone number here'
                 value={formData.phoneNumber}
                 onChange={handleChange}
             />
+            {errors.phoneNumber && <div className='error'>{errors.phoneNumber}</div>}
             </div>
             <div className="form-group">
             <label htmlFor="zipCode" className="form-label">Zip Code:</label>
             <input
-                type="zip"
+                type="text"
                 id="zipCode"
                 className="form-input"
+                placeholder='Enter your zip code name here'
                 value={formData.zipCode}
                 onChange={handleChange}
             />
+            {errors.zipCode && <div className='error'>{errors.zipCode}</div>}
             </div>
-            <button type="submit" className="submit-button">Save Changes</button>
+            <button type="submit" className="submit-button" onClick={handleSubmit}>Save Changes</button>
         </form>
         </div>
     );
