14ebb2a21eccb95ef65f22a10e471e7f2e441413
Keshav Adhikari <k_a354@txstate.edu>
2024-03-01 13:09:50 -0600

    Made some changes in navbar.css and navbar.js

--- client/src/component/NavBar.js
+++ client/src/component/NavBar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 
 const NavBar = () => {
     return (
-<nav class="navbar navbar-expand-lg navbar-white bg-white ">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
   <div class="container-fluid d-flex justify-content-between m-0 w-100"> 
     <Link class="navbar-brand m-0" to = "/"><h1><strong>Health</strong><strong className='vista'>Vista</strong></h1></Link>
     <ul class="navbar-nav m-0">
@@ -13,10 +13,10 @@ const NavBar = () => {
         <a class="nav-link ">Medical Dashboard</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="trackersDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        <a class="nav-link dropdown-toggle bg-dark" href="#" id="trackersDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
           Trackers
         </a>
-        <ul class="dropdown-menu" aria-labelledby="trackersDropdown">
+        <ul class="dropdown-menu bg-dark" aria-labelledby="trackersDropdown">
           <li><a class="dropdown-item" href="#">Tracker 1</a></li>
           <li><a class="dropdown-item" href="#">Tracker 2</a></li>
           <li><a class="dropdown-item" href="#">Tracker 3</a></li>
@@ -33,8 +33,8 @@ const NavBar = () => {
       </li>
     </ul>
     <div className='logins'>
-    <Link to="/signup"><button class="btn btn-outline-success me-2">Signup</button></Link>
-      <Link to="/login"><button class="btn btn-secondary">Sign In</button></Link>
+    <Link to="/signup"><button class="btn btn-outline-light me-2">Sign Up</button></Link>
+      <Link to="/login"><button class="btn btn-dark">Sign In</button></Link>
     </div>
   </div>
 </nav>
