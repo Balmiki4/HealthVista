d062d7490fe81490a46c793222e174d6b60491d6
Balmiki Padhyaya <bfv12@txstate.edu>
2024-03-02 00:39:59 -0600

    Adjusted frontend layout of login page.

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
-import { Link, useHistory } from "react-router-dom";
-import { Alert } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import ForgotPassword from "./forgot";
 import user_icon from "./img/person.png";
 import password_icon from "./img/password.png";
+import "./login.css";
 
 const LoginPage = () => {
-  const [formData, setData] = useState({ username: "", password: "" });
+  const [formData, setFormData] = useState({ username: "", password: "" });
   const [rememberMe, setRememberMe] = useState(false);
   const [errors, setErrors] = useState({});
   const [alertMessage, setAlertMessage] = useState("");
   const [showAlert, setShowAlert] = useState(false);
   const [alertVariant, setAlertVariant] = useState("success");
-  const history = useHistory();
   const [showForgotPassword, setShowForgotPassword] = useState(false);
   const [username, setUsername] = useState("");
   const [statusMessage, setStatusMessage] = useState("");
@@ -87,10 +86,9 @@ const LoginPage = () => {
       console.error("Error:", error.message);
     }
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    // Perform any client-side validation before submitting the form
 
     try {
       const response = await fetch("http://localhost:5000/login", {
@@ -108,11 +106,8 @@ const LoginPage = () => {
       const responseData = await response.json();
 
       if (response.ok) {
-        // Handle successful login
-        // history("/dashboard"); // Redirect to the dashboard or any other page
         displayAlert("success", responseData.message || "Login successful");
       } else {
-        // Handle login failure
         setErrors(responseData.errors || {});
         displayAlert("danger", responseData.message || "Login failed");
       }
@@ -124,85 +119,87 @@ const LoginPage = () => {
 
   return (
     <div className="container">
-      <div className="login-form">
+      <div className="header">
         <div className="text">
           <h1>Welcome back.</h1>
         </div>
+      </div>
 
-        <form onSubmit={handleSubmit}>
-          <div className="inputs">
-            <div className="input">
-              <label htmlFor="username">
-                <img src={user_icon} alt="user name" />
-              </label>
-              <Form.Group>
-                <Form.Control
-                  type="text"
-                  placeholder="Username or Email"
-                  value={formData.username}
-                  onChange={(e) => {
-                    setData({ ...formData, username: e.target.value });
-                    setErrors({ ...errors, username: "" });
-                  }}
-                  isInvalid={!!errors.username}
-                />
-              </Form.Group>
-              {errors.username && (
-                <div className="error-message">{errors.username}</div>
-              )}
-            </div>
-            <div className="input">
-              <label htmlFor="password">
-                <img src={password_icon} alt="password" />
-              </label>
-              <Form.Group>
-                <Form.Control
-                  type="password"
-                  placeholder="Password"
-                  value={formData.password}
-                  onChange={(e) => {
-                    setData({ ...formData, password: e.target.value });
-                    setErrors({ ...errors, password: "" });
-                  }}
-                  isInvalid={!!errors.password}
-                />
-              </Form.Group>
-              {errors.username && (
-                <div className="error-message">{errors.password}</div>
-              )}
-            </div>
-          </div>
-          <div className="login-button">
+      <form onSubmit={handleSubmit}>
+        <div className="inputs">
+          <div className="input">
+            <label htmlFor="username">
+              <img src={user_icon} alt="user name" />
+            </label>
             <Form.Group>
-              <Button
-                className="btn"
-                type="submit"
-                variant="primary"
-                onClick={handleSubmit}
-              >
-                Login
-              </Button>
+              <Form.Control
+                type="text"
+                id="username"
+                placeholder="Username"
+                value={formData.username}
+                onChange={(e) => {
+                  setFormData({ ...formData, username: e.target.value });
+                  setErrors({ ...errors, username: "" });
+                }}
+                isInvalid={!!errors.username}
+              />
             </Form.Group>
+            {errors.username && (
+              <div className="error-message">{errors.username}</div>
+            )}
           </div>
-        </form>
-
-        <div className="remember-forgot">
+          <div className="input">
+            <label htmlFor="password">
+              <img src={password_icon} alt="password" />
+            </label>
+            <Form.Group>
+              <Form.Control
+                type="password"
+                id="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={(e) => {
+                  setFormData({ ...formData, password: e.target.value });
+                  setErrors({ ...errors, password: "" });
+                }}
+                isInvalid={!!errors.password}
+              />
+            </Form.Group>
+            {errors.username && (
+              <div className="error-message">{errors.password}</div>
+            )}
+          </div>
+        </div>
+        <div className="login-button">
           <Form.Group>
-            <Form.Check
-              type="checkbox"
-              label="Remember me"
-              onChange={() => setRememberMe(!rememberMe)}
-              checked={rememberMe}
-            />
+            <Button
+              className="btn"
+              type="submit"
+              variant="primary"
+              onClick={handleSubmit}
+            >
+              Login
+            </Button>
           </Form.Group>
-          <Link to="/forgot" onClick={handleForgotPasswordClick}>
-            Forgot Password?
-          </Link>
         </div>
-        <div className="signup-link">
-          <span>Don't have an account? </span>
-          <Link to="/signup">Sign Up</Link>
-        </div>
+      </form>
+
+      <div className="remember-forgot">
+        <Form.Group>
+          <Form.Check
+            type="checkbox"
+            label="Remember me"
+            onChange={() => setRememberMe(!rememberMe)}
+            checked={rememberMe}
+          />
+        </Form.Group>
+        <Link to="/forgot" onClick={handleForgotPasswordClick}>
+          Forgot Password?
+        </Link>
+      </div>
+      <div className="signup-link">
+        <span>Don't have an account? </span>
+        <Link to="/signup">Sign Up</Link>
       </div>
 
       {/* Forgot Password Form */}
