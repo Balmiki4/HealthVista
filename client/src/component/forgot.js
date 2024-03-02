ed19bb6f224ef53e4f24967c315764b853cb6060
Balmiki Padhyaya <bfv12@txstate.edu>
2024-03-02 01:25:11 -0600

    Added css for "forgot password"

--- client/src/component/forgot.js
+++ client/src/component/forgot.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
+import user_icon from "./img/person.png";
+import password_icon from "./img/password.png";
+import "./forgot.css";
 
 const ForgotPassword = ({ onForgotPassword, onResetPassword }) => {
   const [step, setStep] = useState(1);
   const [username, setUsername] = useState("");
   const [token, setToken] = useState("");
   const [newPassword, setNewPassword] = useState("");
-  const [message, setMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [successMessage, setSuccessMessage] = useState("");
 
@@ -62,10 +64,10 @@ const ForgotPassword = ({ onForgotPassword, onResetPassword }) => {
       if (response.ok) {
         setSuccessMessage(responseData.message);
         setErrorMessage("");
-        // Redirect to the login page after successful password reset
+        // Redirect to the login page after a successful password reset
         setTimeout(() => {
           window.location.href = "/login";
-        }, 3000); // Redirect after 3 seconds (adjust as needed)
+        }, 2000); // Redirect after 2 seconds
       } else {
         setErrorMessage(responseData.error);
         setSuccessMessage("");
@@ -76,64 +78,95 @@ const ForgotPassword = ({ onForgotPassword, onResetPassword }) => {
   };
 
   return (
-    <div>
+    <div className="container">
+      {/* First page of forgot password */}
       {step === 1 && (
         <div>
           <h1>Step 1: Request Token</h1>
-          <Form>
-            <Form.Group controlId="username">
-              <Form.Label>Username:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter your username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant="primary" onClick={handleRequestToken}>
+          <Form className="forgot-form">
+            <div className="inputs">
+              <div className="input">
+                <label htmlFor="username">
+                  <img src={user_icon} alt="user name" />
+                </label>
+                <Form.Group>
+                  <Form.Control
+                    type="text"
+                    id="username"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </Form.Group>
+                {errorMessage && (
+                  <div className="error-message">{errorMessage}</div>
+                )}
+              </div>
+            </div>
+            <Button
+              variant="primary"
+              className="reset-button"
+              onClick={handleRequestToken}
+            >
               Request Token
             </Button>
           </Form>
           <Alert variant="success" show={successMessage !== ""}>
             {successMessage}
           </Alert>
-          <Alert variant="danger" show={errorMessage !== ""}>
-            {errorMessage}
-          </Alert>
         </div>
       )}
-
+      {/* Second page of forgot password */}
       {step === 2 && (
         <div>
           <h1>Step 2: Reset Password</h1>
-          <Form>
-            <Form.Group controlId="token">
-              <Form.Label>Token:</Form.Label>
-              <Form.Control
-                type="text"
-                placeholder="Enter the token you received"
-                value={token}
-                onChange={(e) => setToken(e.target.value)}
-              />
-            </Form.Group>
-            <Form.Group controlId="newPassword">
-              <Form.Label>New Password:</Form.Label>
-              <Form.Control
-                type="password"
-                placeholder="Enter your new password"
-                value={newPassword}
-                onChange={(e) => setNewPassword(e.target.value)}
-              />
-            </Form.Group>
-            <Button variant="primary" onClick={handleResetPassword}>
+          <Form className="forgot-form">
+            <div className="inputs">
+              <div className="input">
+                <label htmlFor="token">
+                  <img src={password_icon} alt="token" />
+                </label>
+                <Form.Group>
+                  <Form.Control
+                    type="text"
+                    id="token"
+                    placeholder="Enter the token you received"
+                    value={token}
+                    onChange={(e) => setToken(e.target.value)}
+                  />
+                </Form.Group>
+                {errorMessage && (
+                  <div className="error-message">{errorMessage}</div>
+                )}
+              </div>
+              <div className="input">
+                <label htmlFor="newPassword">
+                  <img src={password_icon} alt="new password" />
+                </label>
+                <Form.Group>
+                  <Form.Control
+                    type="password"
+                    id="newPassword"
+                    placeholder="Enter your new password"
+                    value={newPassword}
+                    onChange={(e) => setNewPassword(e.target.value)}
+                  />
+                </Form.Group>
+                {errorMessage && (
+                  <div className="error-message">{errorMessage}</div>
+                )}
+              </div>
+            </div>
+            <Button
+              variant="primary"
+              className="reset-button"
+              onClick={handleResetPassword}
+            >
               Reset Password
             </Button>
           </Form>
           <Alert variant="success" show={successMessage !== ""}>
             {successMessage}
-          </Alert>
-          <Alert variant="danger" show={errorMessage !== ""}>
-            {errorMessage}
           </Alert>
         </div>
       )}
