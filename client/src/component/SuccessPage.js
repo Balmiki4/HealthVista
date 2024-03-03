cc9333efd38e587596df919f2c0dd2bfd6a5350f
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-03-02 18:33:23 -0600

    added redirection to createProfile in successpage.js

--- client/src/component/SuccessPage.js
+++ client/src/component/SuccessPage.js
@@ -1,18 +1,50 @@
-import React, { useEffect } from 'react';
-import queryString from 'query-string';
-import { useLocation  } from 'react-router-dom';
+/* eslint-disable no-restricted-globals */
+import React, { useEffect, useState } from "react";
+import { useHistory, Link } from "react-router-dom";
 
 function SuccessPage() {
-  
-    return (
-      <div className='conainer feature ct1'>
+  const history = useHistory();
+  const [showLink, setShowLink] = useState(true);
+  const [countdown, setCountdown] = useState(6);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (countdown > 0) {
+      intervalId = setInterval(() => {
+        setCountdown(countdown - 1);
+      }, 1000);
+    } else {
+      history.push("/createProfile");
+    }
+
+    return () => {
+      clearInterval(intervalId);    };
+  }, [history, countdown]);
+
+  return (
+    <div className="conainer feature ct1">
       <div class="heading">
         <h1 className="secondary-heading">Payment Successful!</h1>
-        <h3 className="font-only-heading">Your subscription has been updated. Thank you!</h3>
+        <h3 className="font-only-heading">
+          Your subscription has been updated. Thank you!
+        </h3>
         <div class="separator"></div>
+        {countdown > 0 ? (
+          <p>Redirecting in {countdown}...</p>
+        ) : (
+          <p>Redirecting in </p>
+        )}
+
+        {showLink && (
+          <p>
+            Click <Link to="/createProfile">here</Link> if you are not
+            redirected automatically.
+          </p>
+        )}
       </div>
-      </div>
-    );
-  }
-  
-  export default SuccessPage;
+    </div>
+  );
+}
+
+export default SuccessPage;
