ea55163fafe5a7e09fffdd3a3e790700f9d02678
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-03-03 21:51:37 -0600

    fixed footer

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -2,7 +2,7 @@ const Footer =()=>{
     return(
         <footer class="footer navbar-dark bg-dark ">
         <div>
-          <div class="col-md-12 mt-4 text-center">
+          <div class="col-md-12 text-center">
             <p class="footer__copyright">HealthVISTA &#169; 2024</p>
           </div>
         </div>
