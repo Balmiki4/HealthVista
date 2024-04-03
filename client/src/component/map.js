d56713b575e2b79f5240d30d4bd2f60d0df57564
Balmiki Padhyaya <bfv12@txstate.edu>
2024-04-03 14:59:30 -0500

    Added star rating for hospitals

--- client/src/component/map.js
+++ client/src/component/map.js
@@ -103,7 +103,10 @@ const Map = () => {
           "vicinity",
           "place_id",
           "icon",
-          "url",
+          "URL",
+          "opening_hours",
+          "rating",
+          "adrFormatAddress",
           "formatted_phone_number",
           "website",
         ], // Include the website field
@@ -210,6 +213,29 @@ const Map = () => {
     }
   };
 
+  //render stars for rating
+  const renderStars = (rating) => {
+    const fullStars = Math.floor(rating);
+    const hasHalfStar = rating % 1 >= 0.5;
+    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0);
+
+    const stars = [];
+
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(<span key={`full-${i}`} className="star filled"></span>);
+    }
+
+    if (hasHalfStar) {
+      stars.push(<span key="half" className="star half"></span>);
+    }
+
+    for (let i = 0; i < emptyStars; i++) {
+      stars.push(<span key={`empty-${i}`} className="star empty"></span>);
+    }
+
+    return stars;
+  };
+
   return (
     <div className="map-container">
       <div className="search-container">
@@ -245,7 +271,7 @@ const Map = () => {
                 <li key={index}>
                   <strong>
                     <a
-                      href={hospital.url}
+                      href={hospital.websiteURI}
                       target="_blank"
                       rel="noopener noreferrer"
                     >
@@ -253,6 +279,7 @@ const Map = () => {
                     </a>
                     <img src={hospital_icon} alt="hospital icon" />
                   </strong>
+
                   <p>
                     Distance:{" "}
                     {hospital.geometry && userLocation
@@ -265,6 +292,10 @@ const Map = () => {
                       : "N/A"}
                   </p>
                   <p>Address: {hospital.vicinity}</p>
+                  <div className="rating-container">
+                    <p>Rating:</p>
+                    <div className="rating">{renderStars(hospital.rating)}</div>
+                  </div>
 
                   {hospital.formatted_phone_number && (
                     <p>Phone: {hospital.formatted_phone_number}</p>
