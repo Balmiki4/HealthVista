ac0bda04f9f16d3ab288aa16905f62ac32fcbb24
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-04 13:30:39 -0500

    designed Nutrition page using css and bootsrtrap

--- client/src/component/Nutrition/Nutrition.js
+++ client/src/component/Nutrition/Nutrition.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
+import "./Nutrition.css";
 
 const Nutrition = () => {
   return (
-    <div className="Nutritioncontainer">
-      <h1 className="text-center my-4">Nutrition Tracker</h1>
+    <div className="Nutritioncontainer ">
+      <div className="header">
+        <div className="text">
+          <h1 className="font-only-heading">Nutrition Tracker</h1>
+        </div>
+        <div className="underline"></div>
+      </div>
       <div className="row">
         <div className="col-md-3">
-          <div className="card">
+          <div className="card shadow-none">
             <div className="card-header">Today's Food Log</div>
             <ul className="list-group list-group-flush">
               {/* Food log items will show up here*/}
@@ -14,7 +20,7 @@ const Nutrition = () => {
           </div>
         </div>
         <div className="col-md-3">
-          <div className="card">
+          <div className="card shadow-none">
             <div className="card-header">Add Food</div>
             <div className="card-body">
               <div className="mb-3">
@@ -23,31 +29,28 @@ const Nutrition = () => {
                   className="form-control"
                   placeholder="Search for a food..."
                 />
-                <button className="btn btn-success mt-2">
-                  Search
-                </button>
+                <button className="btn btn-success mt-2">Search</button>
               </div>
               {/* Search results will show up here*/}
             </div>
           </div>
         </div>
         <div className="col-md-3">
-          <div className="card">
+          <div className="card shadow-none">
             <div className="card-header">Nutrition Analysis</div>
             <div className="card-body">
               <p>Total Calories: </p>
               <p>Total Protein: </p>
               <p>Total Carbs: </p>
+              <p>Total Fats: </p>
             </div>
           </div>
         </div>
         <div className="col-md-3">
-          <div className="card">
+          <div className="card shadow-none">
             <div className="card-header d-flex justify-content-between align-items-center">
               Past Logs
-              <button className="btn btn-success btn-sm">
-                Show
-              </button>
+              <button className="btn btn-success btn-sm">Show</button>
             </div>
             {/* Past logs will up here */}
           </div>
