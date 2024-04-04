ee40db3b43b34471fbe2cdc1342d8a4488f2a8a7
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-04 17:39:18 -0500

    Added routes to add and delete food

--- server/nutrition_python/nutrition_routes.py
+++ server/nutrition_python/nutrition_routes.py
@@ -9,7 +9,23 @@ nutrition_bp = Blueprint('nutrition_routes', __name__, url_prefix='/api')
 # In-memory list to store added foods will change once i implement mongodb
 foods = []
 
-# Route to add food will upadte once i implement mongodb
+# Route to get food will upadte once i implement mongodb
 @nutrition_bp.route('/foods', methods=['GET'])
 def get_foods():
     return jsonify(foods)
+
+# Route to add food will update once i implement mongodb
+@nutrition_bp.route('/foods', methods=['POST'])
+def add_food():
+    food_data = request.get_json()
+    foods.append(food_data)
+    return jsonify({'message': 'Food added successfully'})
+
+# Route to delete food will update once i implement mongodb
+@nutrition_bp.route('/foods/<food_name>', methods=['DELETE'])
+def delete_food(food_name):
+    for food in foods:
+        if food['name'] == food_name:
+            foods.remove(food)
+            return jsonify({'message': f'{food_name} deleted'})
+    return jsonify({'error': f'{food_name} not found'}), 404
