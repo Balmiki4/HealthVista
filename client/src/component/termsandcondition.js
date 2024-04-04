f0b036d1e28be00caba6216c444bb2070954c58b
Keshav Adhikari <k_a354@txstate.edu>
2024-04-04 09:42:39 -0500

    Created a termsandcondition.js for termsandcondition page

---
+++ client/src/component/termsandcondition.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+class TermsAndConditions extends React.Component {
+    render() {
+        return (
+            <div className="container">
+            <h1>Terms and Conditions</h1>
+            <p>Welcome to Health Vista, your ultimate health management app. By accessing or using Health Vista, you agree to comply with and be bound by the following terms and conditions of use. Please review these terms carefully before using our app. If you do not agree with any part of these terms, you may not access or use Health Vista.</p>
+       
+                
