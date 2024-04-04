5159b23068e9f368983546d116cf587c97c368f4
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-04 17:44:55 -0500

    added route to get nutrition data from api

--- server/nutrition_python/nutrition_routes.py
+++ server/nutrition_python/nutrition_routes.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 
 nutrition_bp = Blueprint('nutrition_routes', __name__, url_prefix='/api')
 
+api_key = os.environ.get("NUT_API_KEY")
+
 # In-memory list to store added foods will change once i implement mongodb
 foods = []
 
@@ -29,3 +31,15 @@ def delete_food(food_name):
             foods.remove(food)
             return jsonify({'message': f'{food_name} deleted'})
     return jsonify({'error': f'{food_name} not found'}), 404
+
+# Route to get nutrition data from api
+@nutrition_bp.route('/nutrition', methods=['POST'])
+def get_nutrition_data():
+    query = request.get_json().get('query')
+    api_url = f'https://api.api-ninjas.com/v1/nutrition?query={query}'
+    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+
+    if response.status_code == requests.codes.ok:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': f'Error: {response.status_code} - {response.text}'}), response.status_code
