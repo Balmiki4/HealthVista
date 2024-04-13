15bd784190b12d4d8960b81e3522b9b31ea9b621
Balmiki Padhyaya <bfv12@txstate.edu>
2024-04-12 22:31:59 -0500

    Added css for the footer

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,42 +1,56 @@
+import React from "react";
 import facebook from "./img/icons/facebook.svg";
 import twitter from "./img/icons/twitterx.svg";
 import instagram from "./img/icons/instagram.svg";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer class="footer navbar-dark bg-dark mt-0 ">
-      <div>
-        <div class="col-md-12 text-center">
-          <p class="footer__copyright">HealthVISTA &#169; 2024</p>
+    <footer className="footer bg-dark text-white py-4">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-md-6 mb-3 mb-md-0">
+            <p className="mb-0 footer__copyright">HealthVISTA &copy; 2024</p>
+          </div>
+          <div className="col-md-6">
+            <div className="d-flex justify-content-md-end social-icons">
+              <a
+                href="https://www.facebook.com/profile.php?id=61557977626802"
+                target="_blank"
+                rel="noreferrer"
+                className="text-white mr-3"
+              >
+                <img src={facebook} alt="Facebook" />
+              </a>
+              <a
+                href="https://twitter.com/HealthVistas"
+                target="_blank"
+                rel="noreferrer"
+                className="text-white mr-3"
+              >
+                <img src={twitter} alt="Twitter" />
+              </a>
+              <a
+                href="https://www.instagram.com/health_vista/"
+                target="_blank"
+                rel="noreferrer"
+                className="text-white"
+              >
+                <img src={instagram} alt="Instagram" />
+              </a>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="footer-container">
-        <div className="social-icons">
-          <a
-            href="https://www.facebook.com/profile.php?id=61557977626802"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={facebook} alt="Facebook" />
-          </a>
-          <a
-            href="https://twitter.com/HealthVistas"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={twitter} alt="Twitter" />
-          </a>
-          <a
-            href="https://www.instagram.com/health_vista/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <img src={instagram} alt="Instagram" />
-          </a>
-        </div>
-        <div className="legal-info">
-          <a href="privacy-policy.html">Privacy Policy</a>
-          <a href="terms-of-service.html">Terms of Service</a>
+        <div className="row mt-3">
+          <div className="col-12 d-flex justify-content-center legal-info">
+            <a href="privacy-policy.html" className="text-white mr-3">
+              Privacy Policy
+            </a>
+            <div className="footer-separator"></div>
+            <a href="terms-of-service.html" className="text-white ml-3">
+              Terms of Service
+            </a>
+          </div>
         </div>
       </div>
     </footer>
