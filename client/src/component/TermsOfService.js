c57b607dbb5569f596e61e3136af9088ea9064c1
Balmiki Padhyaya <bfv12@txstate.edu>
2024-04-12 23:26:25 -0500

    Added files for privacy policy and terms of service

---
+++ client/src/component/TermsOfService.js
@@ -0,0 +1,12 @@
+// TermsOfService.js
+import React from "react";
+
+const TermsOfService = () => {
+  return (
+    <div>
+      <h1>Terms of Service</h1>
+    </div>
+  );
+};
+
+export default TermsOfService;
