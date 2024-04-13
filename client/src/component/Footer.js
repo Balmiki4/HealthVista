80ed1a544a700e17238fffa6f8504a4a8f18c7b2
Balmiki Padhyaya <bfv12@txstate.edu>
2024-04-12 21:44:46 -0500

    transferred " footer" code to the designated file

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,13 +1,46 @@
-const Footer =()=>{
-    return(
-        <footer class="footer navbar-dark bg-dark mt-0 ">
-        <div>
-          <div class="col-md-12 text-center">
-            <p class="footer__copyright">HealthVISTA &#169; 2024</p>
-          </div>
-        </div>
-      </footer>
-    );
-}
+import facebook from "./img/icons/facebook.svg";
+import twitter from "./img/icons/twitterx.svg";
+import instagram from "./img/icons/instagram.svg";
 
-export default Footer
+const Footer = () => {
+  return (
+    <footer class="footer navbar-dark bg-dark mt-0 ">
+      <div>
+        <div class="col-md-12 text-center">
+          <p class="footer__copyright">HealthVISTA &#169; 2024</p>
+        </div>
+      </div>
+      <div className="footer-container">
+        <div className="social-icons">
+          <a
+            href="https://www.facebook.com/profile.php?id=61557977626802"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebook} alt="Facebook" />
+          </a>
+          <a
+            href="https://twitter.com/HealthVistas"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={twitter} alt="Twitter" />
+          </a>
+          <a
+            href="https://www.instagram.com/health_vista/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instagram} alt="Instagram" />
+          </a>
+        </div>
+        <div className="legal-info">
+          <a href="privacy-policy.html">Privacy Policy</a>
+          <a href="terms-of-service.html">Terms of Service</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- client/src/component/homePage.js
+++ client/src/component/homePage.js
@@ -4,9 +4,6 @@ import vista from "./img/chatbot.svg";
 import wellness from "./img/wellness.jpg";
 import "./homePage.css";
 import PopupForm from "./PopupFrom";
-import facebook from "./img/icons/facebook.svg";
-import twitter from "./img/icons/twitterx.svg";
-import instagram from "./img/icons/instagram.svg";
 
 const homePage = () => {
   return (
@@ -259,38 +256,6 @@ const homePage = () => {
       <div>
         <PopupForm />
       </div>
-
-      <footer>
-        <div className="footer-container">
-          <div className="social-icons">
-            <a
-              href="https://www.facebook.com/profile.php?id=61557977626802"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={facebook} alt="Facebook" />
-            </a>
-            <a
-              href="https://twitter.com/HealthVistas"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={twitter} alt="Twitter" />
-            </a>
-            <a
-              href="https://www.instagram.com/health_vista/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={instagram} alt="Instagram" />
-            </a>
-          </div>
-          <div className="legal-info">
-            <a href="privacy-policy.html">Privacy Policy</a>
-            <a href="terms-of-service.html">Terms of Service</a>
-          </div>
-        </div>
-      </footer>
     </div>
   );
 };
