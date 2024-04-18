b9c3cbbf615d40a56837c6f75ee80a9f029bb4e8
Keshav Adhikari <k_a354@txstate.edu>
2024-04-17 20:47:00 -0500

    Added CSS for PlanDetails page

--- client/src/component/PlanDetails.js
+++ client/src/component/PlanDetails.js
@@ -1,5 +1,7 @@
+
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "./planDetails.css"; 
 
 const PlanDetails = () => {
   const location = useLocation();
@@ -10,79 +12,63 @@ const PlanDetails = () => {
   }
 
   return (
-    <div className="conainer">
+    <div className="container">
       {planData.map((plan, index) => (
-        <div key={index}>
+        <div key={index} className="plan-details-card">
           <h3 className="secondary-heading">{plan.name}</h3>
-          <p>Premium: ${plan.premium.toFixed(2)}</p>
+          <p className="plan-price">Premium: ${plan.premium.toFixed(2)}</p>
           <p>Medical Deductible: ${plan.deductibles[0].amount}</p>
           {plan.deductibles.length > 1 && (
             <p>Drug Deductible: ${plan.deductibles[1].amount}</p>
           )}
           <p>Maximum Out-of-Pocket Costs (MOOPs): ${plan.moops[0].amount}</p>
-          <p>Quality Ratings:</p>
-          <ul>
+          <p className="quality-ratings-heading">Quality Ratings:</p>
+          <ul className="quality-ratings-list">
             <li>
               Global Rating:{" "}
               {plan.quality_rating.global_rating > 0
                 ? plan.quality_rating.global_rating
                 : "Rating not available"}
             </li>
-            <li>
-              Clinical Quality Management Rating:{" "}
-              {plan.quality_rating.clinical_quality_management_rating > 0
-                ? plan.quality_rating.clinical_quality_management_rating
-                : "Rating not available"}
-            </li>
-            <li>
-              Enrollee Experience Rating:{" "}
-              {plan.quality_rating.enrollee_experience_rating > 0
-                ? plan.quality_rating.enrollee_experience_rating
-                : "Rating not available"}
-            </li>
-            <li>
-              Plan Efficiency Rating:{" "}
-              {plan.quality_rating.plan_efficiency_rating > 0
-                ? plan.quality_rating.plan_efficiency_rating
-                : "Rating not available"}
-            </li>
           </ul>
-          <p>
-            <a
-              href={plan.benefits_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Benefits URL
-            </a>
-          </p>
-          <p>
-            <a
-              href={plan.brochure_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Brochure URL
-            </a>
-          </p>
-          <p>
-            <a
-              href={plan.formulary_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Formulary URL
-            </a>
-          </p>
-          <p>
-            <a
-              href={plan.network_url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Network URL
-            </a>
-          </p>
+          <div className="url-links">
+            <p>
+              <a
+                href={plan.benefits_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Benefits URL
+              </a>
+            </p>
+            <p>
+              <a
+                href={plan.brochure_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Brochure URL
+              </a>
+            </p>
+            <p>
+              <a
+                href={plan.formulary_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Formulary URL
+              </a>
+            </p>
+            <p>
+              <a
+                href={plan.network_url}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Network URL
+              </a>
+            </p>
+          </div>
         </div>
       ))}
     </div>
