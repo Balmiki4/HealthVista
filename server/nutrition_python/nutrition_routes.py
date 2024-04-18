f1e63d58ec8106c53a0d6d732bb4b76a1cc5c8e8
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-18 13:49:54 -0500

    Implemented MongoDB integration, authentication for user, and updated routes

--- server/nutrition_python/nutrition_routes.py
+++ server/nutrition_python/nutrition_routes.py
@@ -1,6 +1,11 @@
 import os
 from flask import Blueprint, Flask, jsonify, request
 import requests
+from flask_cors import cross_origin
+from jose import jwt
+from pymongo import MongoClient
+from bson import ObjectId
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -8,32 +13,74 @@ nutrition_bp = Blueprint('nutrition_routes', __name__, url_prefix='/api')
 
 api_key = os.environ.get("NUT_API_KEY")
 
-# In-memory list to store added foods will change once i implement mongodb
-foods = []
+uri = os.getenv('MONGO_URI')
+client = MongoClient(uri)
+db = client['healthvista']
+users_collection = db['userdata']
 
-# Route to get food will upadte once i implement mongodb
+
+#requires_auth decorator to check if the user is authenticated or not
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        user_id = request.headers.get('User-Id')
+        access_token = request.headers.get('Authorization')
+
+        if not user_id or not access_token:
+            return jsonify({'error': 'Missing user_id or access_token'}), 401
+
+        try:
+            decoded_token = jwt.decode(access_token.replace('Bearer ', ''), "SECRET_KEY", algorithms=["HS256"])
+            if decoded_token['user_id'] != user_id:
+                return jsonify({'error': 'Invalid access token'}), 401
+        except jwt.ExpiredSignatureError:
+            return jsonify({'error': 'Access token expired'}), 401
+        except jwt.JWTError:
+            return jsonify({'error': 'Invalid access token'}), 401
+
+        return f(*args, **kwargs)
+    return decorated
+
+
 @nutrition_bp.route('/foods', methods=['GET'])
+@cross_origin(origins=['http://localhost:3000'], supports_credentials=True)
+@requires_auth
 def get_foods():
-    return jsonify(foods)
+    user_id = ObjectId(request.headers.get('User-Id'))
+    user_data = users_collection.find_one({'_id': user_id})
+    if user_data:
+        foods = user_data.get('food_data', [])
+        return jsonify(foods)
+    else:
+        return jsonify({'foods': []}), 200
 
-# Route to add food will update once i implement mongodb
 @nutrition_bp.route('/foods', methods=['POST'])
+@cross_origin(origins=['http://localhost:3000'], supports_credentials=True)
+@requires_auth
 def add_food():
-    food_data = request.get_json()
-    foods.append(food_data)
-    return jsonify({'message': 'Food added successfully'})
+    food_data = request.json
+    user_id = ObjectId(request.headers.get('User-Id'))
+    result = users_collection.update_one(
+        {'_id': user_id},
+        {'$push': {'food_data': food_data}}
+    )
+    return jsonify({'message': 'Food added successfully'}), 200
 
-# Route to delete food will update once i implement mongodb
 @nutrition_bp.route('/foods/<food_name>', methods=['DELETE'])
-def delete_food(food_name):
-    for food in foods:
-        if food['name'] == food_name:
-            foods.remove(food)
-            return jsonify({'message': f'{food_name} deleted'})
-    return jsonify({'error': f'{food_name} not found'}), 404
+@requires_auth
+def delete_food_by_id(food_name):
+    user_id = ObjectId(request.headers.get('User-Id'))
+    result = users_collection.update_one(
+        {'_id': user_id},
+        {'$pull': {'food_data': {'name': food_name}}}
+    )
+    return jsonify({'message': 'Food deleted successfully'}), 200
 
 # Route to get nutrition data from api
 @nutrition_bp.route('/nutrition', methods=['POST'])
+@cross_origin(origins=['http://localhost:3000'], supports_credentials=True)
+@requires_auth
 def get_nutrition_data():
     query = request.get_json().get('query')
     api_url = f'https://api.api-ninjas.com/v1/nutrition?query={query}'
