be7405776eebe9beab752084b49a6ca845219a57
Muhammad Qassim <muhammadqassim92@gmail.com>
2024-04-19 20:49:33 -0500

    Updated the payment plan page to have only 2 tiers

--- client/src/component/PaymentPlan.js
+++ client/src/component/PaymentPlan.js
@@ -60,33 +60,35 @@ function PaymentPlan() {
   };
   return (
     <div class="conainer feature">
-      <div class="block conainer block-plans" style={{ paddingTop: "0px" }}>
-        <div class="row">
-          <div class="heading">
-            <h1 className="secondary-heading">Thank you for registering</h1>
-            <h3 className="font-only-heading">Please choose a plan</h3>
-            <div class="separator"></div>
-          </div>
-          <div class="grid grid--1x3">
-            <div class="plan">
-              <div class="card card--secondary">
-                <header class="card__header">
-                  <h3 class="plan__name">Free Tier</h3>
-                  <span class="plan__price">$0</span>
-                  <span class="plan__billing-cycle">/month</span>
+              <div class="block conainer block-plans pt-0 pb-0">
+          <div class="row">
+            <div class="heading">
+              <h1 className="secondary-heading">Subscription Plans</h1>
+              <h5 className="font-only-heading">
+                Flexible Options to Suit Your Needs
+              </h5>
+              <div class="separator"></div>
+            </div>
+            <div class="grid grid--1x3">
+              <div class="plan">
+                <div class="card card--secondary">
+                  <header class="card__header">
+                    <h3 class="plan__name">Free Tier</h3>
+                    <span class="plan__price">$0</span>
+                    <span class="plan__billing-cycle">/month</span>
 
-                  <span class="plan__description">Discovery</span>
-                </header>
-                <div class="card__body">
-                  <ul class="list list--tick">
-                    <li class="list__item">Medical record tracking</li>
-                    <li class="list__item">Appointment reminders</li>
-                    <li class="list__item">
-                      Limited access to wellness videos
-                    </li>
-                    <li class="list__item">Basic chatbot support</li>
-                  </ul>
-                  <div className="plan-btn">
+                    <span class="plan__description">Discovery</span>
+                  </header>
+                  <div class="card__body">
+                    <ul class="list list--tick">
+                      <li class="list__item">Medication Tracker</li>
+                      <li class="list__item">Hospital Tracker</li>
+                      <li class="list__item">
+                      Exercise Videos
+                      </li>
+                      <li class="list__item">Health Articles</li>
+                    </ul>
+                    <div className="plan-btn">
                     <Link to={`/createProfile?customerId=${customerId}`}>
                       <button
                         class="btn btn-outline-success plan-buy-now-btn"
@@ -96,31 +98,31 @@ function PaymentPlan() {
                       </button>
                     </Link>
                   </div>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <div class="plan plan--popular">
-                <div class="card card--primary">
-                  <header class="card__header">
-                    <h3 class="plan__name">Pro Tier</h3>
-                    <span class="plan__price">$15</span>
-                    <span class="plan__billing-cycle">/month</span>
+              <div>
+                <div class="plan plan--popular">
+                  <div class="card card--primary">
+                    <header class="card__header">
+                      <h3 class="plan__name">Pro Tier</h3>
+                      <span class="plan__price">$2.99</span>
+                      <span class="plan__billing-cycle">/month</span>
 
-                    <span class="plan__description">Premium</span>
-                  </header>
-                  <div class="card__body">
-                    <ul class="list list--tick">
-                      <li class="list__item">Unlimited medical records</li>
-                      <li class="list__item">
-                        Healthcare provider integration
-                      </li>
-                      <li class="list__item">
-                        Personalized wellness recommendations
-                      </li>
-                      <li class="list__item">Virutal Therapist</li>
-                    </ul>
-                    <div className="plan-btn">
+                      <span class="plan__description">Premium</span>
+                    </header>
+                    <div class="card__body">
+                      <ul class="list list--tick">
+                        <li class="list__item">Vista (AI Therapist)</li>
+                        <li class="list__item">
+                        Nutrition Tracker
+                        </li>
+                        <li class="list__item">
+                          Insurance Recommender
+                        </li>
+                        <li class="list__item">24/7 Customer Service</li>
+                      </ul>
+                      <div className="plan-btn">
                       <button
                         class="btn btn-outline-success plan-buy-now-btn"
                         onClick={() => handlePayment("Pro tier")}
@@ -128,45 +130,16 @@ function PaymentPlan() {
                         Buy Now
                       </button>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div class="plan">
-                <div class="card card--secondary">
-                  <header class="card__header">
-                    <h3 class="plan__name">Basic Teir</h3>
-                    <span class="plan__price">$5</span>
-                    <span class="plan__billing-cycle">/month</span>
-
-                    <span class="plan__description">Essential</span>
-                  </header>
-                  <div class="card__body">
-                    <ul class="list list--tick">
-                      <li class="list__item">Unlimited medical records</li>
-                      <li class="list__item">
-                        Customizable medication reminders
-                      </li>
-                      <li class="list__item">Full access to wellness videos</li>
-                      <li class="list__item">Expanded tracker options</li>
-                    </ul>
-                    <div className="plan-btn">
-                      <button
-                        class="btn btn-outline-success plan-buy-now-btn"
-                        onClick={() => handlePayment("Basic tier")}
-                      >
-                        Buy Now
-                      </button>
                     </div>
                   </div>
                 </div>
+              </div>
+              <div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
   );
 }
 
