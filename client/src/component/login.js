a536eed5dbb15d316d8b3b730ba6e0765f018466
Balmiki Padhyaya <bfv12@txstate.edu>
2024-04-20 22:32:33 -0500

    Fixed user redirection after login

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -10,12 +10,12 @@ import { Alert } from "react-bootstrap";
 
 const LoginPage = () => {
   const [formData, setData] = useState({ username: "", password: "" });
+  const history = useHistory();
   const [rememberMe, setRememberMe] = useState(false);
   const [errors, setErrors] = useState({});
   const [alertMessage, setAlertMessage] = useState("");
   const [showAlert, setShowAlert] = useState(false);
   const [alertVariant, setAlertVariant] = useState("success"); // Default to success
-  const history = useHistory();
 
   const handleAlertClose = () => {
     setShowAlert(false);
@@ -61,20 +61,16 @@ const LoginPage = () => {
       console.log("Login successful");
 
       const user_id = data.user_id || null;
-      console.log("User Plan:", user_id);
       const access_token = data.access_token || null;
-      const user_plan = data.user_plan || null;
-      console.log("User Plan:", user_plan);
-
+      const username = data.username || null;
       // Store session data in sessionStorage or localStorage
       sessionStorage.setItem("user_id", user_id);
       sessionStorage.setItem("access_token", access_token);
-
-      sessionStorage.setItem("user_plan", user_plan);
+      sessionStorage.setItem("username", username);
 
       // Clear the form fields upon successful login
       setData({ username: "", password: "" });
-      history.push("/vista");
+
       // Show success alert
       displayAlert("success", "Login successful");
     } else if (response.status === 401) {
@@ -88,11 +84,11 @@ const LoginPage = () => {
       setData({ username: "", password: "" });
       displayAlert("danger", "Login failed. Invalid username or password");
     }
-        if (response.ok) {
-        // Reload the page
-          history.push("/Dashboard"); 
-          window.location.reload();
-        }
+    if (response.ok) {
+      // Reload the page
+      history.push("/Dashboard");
+      window.location.reload();
+    }
   };
   return (
     <div className="container ct">
